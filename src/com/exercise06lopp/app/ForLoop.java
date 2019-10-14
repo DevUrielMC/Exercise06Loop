e8067d2b352296af557739c161aa80b848df8fe1
DevUrielMC <urimarcervantes.95@gmail.com>
2019-10-14 13:05:52 -0500

    The firsh exercise of loops

--- src/com/exercise06lopp/app/ForLoop.java
+++ src/com/exercise06lopp/app/ForLoop.java
@@ -1,10 +1,41 @@
 package com.exercise06lopp.app;
 
-public class ForLoop {
+public class ForLoop 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		//Constant declaration
+		// variable declaration
+		//objets construction
+		
+		//create a serie of 1 - 100 1by
+		for(int i=100;i>=1;i--)
+		{
+			System.out.println(i);
+		}
+		for(int i =0; i<=100;i+=2)
+		{
+			System.out.println(i);
+		}
+		System.out.println("Display odd numbers from 100 to 200");
+		for(int i =100; i<200;i++)
+		{
+			if(i%2==0)
+			{				//break rompe el ciclo
+				continue;//rompe el flujo del ciclo y se va a la evaluacion del ciclo
+			}
+			System.out.println(i);
+		}
+		System.out.println("Display just the half of the secuence 100 to 200");
+			for(int i=100;i<200;i++)
+			{
+				System.out.println(i);
+				if(i==150);
+				{
+					break;
+				}
+			}
 	}
 
 }
